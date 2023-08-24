4b3e6f7cb1bd5b9fcdf4ed7c8f4a64de8937b727
haritha-25 <114257262+haritha-25@users.noreply.github.com>
2023-08-24 10:58:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import  Home  from './component/Home';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './component/About';
+import sidenav from './Sidenav';
+import BookList from './component/BookList';
+import AddBook from './component/AddBook';
+import EditBook from './component/EditBook';
+import Sidenav from './Sidenav';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidenav/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/books' element={<BookList />} />
+        <Route path='/addbook' element={<AddBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
